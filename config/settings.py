aaf2a435928c2fc502c1a1752d8a74424dda1b0a
Bol-F <bolievfirdavs0@gmail.com>
2025-05-14 15:21:09 +0500

    settings

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware' 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,8 +57,6 @@ MIDDLEWARE = [
     # LOCAL
     'users.middleware.LogAgent',
     'users.middleware.LogIPMiddleware',
-    'django.middleware.locale.LocaleMiddleware',  # Tilni
-    'django.middleware.common.CommonMiddleware',  # Asosiy
 ]
 
 ROOT_URLCONF = 'config.urls'
